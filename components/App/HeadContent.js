cbd875838c36ae465820e57cea5da44f64a236b2
Itamar Cohen <itamar.c@taboola.com>
2020-09-13 12:21:16 +0300

    Init Commit

---
+++ components/App/HeadContent.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const HeadContent = () => (
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charSet="utf-8" />
+    <meta name="theme-color" content="#ffffff" />
+    <meta name="description" content="Photify - edit your photos easily." />
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" />
+    <title>Photify</title>
+  </Head>
+);
+
+export default HeadContent;
