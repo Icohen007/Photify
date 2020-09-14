c0b5b3a7ba2562fed2a11b5ec2d83d6097d37348
Itamar Cohen <itamar.c@taboola.com>
2020-09-14 12:23:05 +0300

    Slider Component

---
+++ components/Slider.js
@@ -0,0 +1,140 @@
+import styled from 'styled-components';
+import { useCallback, useEffect, useState } from 'react';
+import Filter from './Filter';
+
+const StyledSlider = styled.div`
+height: 100%;
+width: 200rem;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`;
+
+const filters = [
+  {
+    description: 'Blur',
+    filterType: 'blur',
+    range: {
+      min: 0,
+      max: 100,
+      step: 1,
+      defaultValue: 0,
+    },
+  },
+  {
+    description: 'Grayscale',
+    filterType: 'grayscale',
+    range: {
+      min: 0,
+      max: 1,
+      step: 0.01,
+      defaultValue: 0,
+    },
+  },
+  {
+    description: 'Invert',
+    filterType: 'invert',
+    range: {
+      min: 0,
+      max: 1,
+      step: 0.01,
+      defaultValue: 0,
+    },
+  },
+  {
+    description: 'Sepia',
+    filterType: 'sepia',
+    range: {
+      min: 0,
+      max: 1,
+      step: 0.01,
+      defaultValue: 0,
+    },
+  },
+  {
+    description: 'Hue-rotate',
+    filterType: 'hue-rotate',
+    range: {
+      min: 0,
+      max: 360,
+      step: 1,
+      defaultValue: 0,
+    },
+  },
+  {
+    description: 'Opacity',
+    filterType: 'opacity',
+    range: {
+      min: 0,
+      max: 1,
+      step: 0.01,
+      defaultValue: 1,
+    },
+  },
+  {
+    description: 'Brightness',
+    filterType: 'brightness',
+    range: {
+      min: 0,
+      max: 2,
+      step: 0.01,
+      defaultValue: 1,
+    },
+  },
+  {
+    description: 'Contrast',
+    filterType: 'contrast',
+    range: {
+      min: 0,
+      max: 2,
+      step: 0.01,
+      defaultValue: 1,
+    },
+  },
+  {
+    description: 'Saturation',
+    filterType: 'saturate',
+    range: {
+      min: 0,
+      max: 2,
+      step: 0.01,
+      defaultValue: 1,
+    },
+  },
+];
+
+const filterValuesObject = filters.reduce((acc, cur) => {
+  acc[cur.filterType] = cur.range.defaultValue;
+  return acc;
+}, {});
+
+const Slider = ({ setFilterString }) => {
+  const [filterValues, setFilterValues] = useState(filterValuesObject);
+  console.log(filterValues);
+
+  useEffect(() => {
+    const filterString = `blur(${filterValues.blur}px) brightness(${filterValues.brightness}) contrast(${filterValues.contrast}) grayscale(${filterValues.grayscale}) invert(${filterValues.invert}) opacity(${filterValues.opacity}) saturate(${filterValues.saturate}) sepia(${filterValues.sepia}) hue-rotate(${filterValues['hue-rotate']}deg)`;
+    setFilterString(filterString);
+  }, [filterValues]);
+
+  const handleFilterChange = useCallback(({ target }) => {
+    setFilterValues((prevFilterValues) => ({ ...prevFilterValues, [target.id]: +target.value }));
+  }, [filterValues]);
+
+  return (
+    <StyledSlider>
+      <h2> Filters:</h2>
+      {filters.map((filterObj) => (
+        <Filter
+          {...filterObj}
+          key={filterObj.filterType}
+          value={filterValues[filterObj.filterType]}
+          onChange={handleFilterChange}
+        />
+      ))}
+    </StyledSlider>
+  );
+};
+
+export default Slider;
