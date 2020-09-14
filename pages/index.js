51f8bb53568e71e139858b4e20a3c77d6db71bc5
Itamar Cohen <itamar.c@taboola.com>
2020-09-14 13:09:09 +0300

    Basic functionality

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,136 @@
+import styled from 'styled-components';
+import { useEffect, useRef, useState } from 'react';
+import Slider from '../components/Slider';
+
+const Container = styled.div`
+display: flex;
+width: 100%;
+min-height: 100vh;
+`;
+
+const ImageContainer = styled.div`
+width: 80%;
+`;
+
+function useStateRef(initialValue) {
+  const [value, setValue] = useState(initialValue);
+  const ref = useRef(value);
+
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+
+  return [value, setValue, ref];
+}
+
 export default function Home() {
-  return <div> hey </div>;
+  const dropZoneRef = useRef(null);
+  const canvasRef = useRef(null);
+  const imageRef = useRef(null);
+
+  const [filterString, setFilterString, filterStringRef] = useStateRef('');
+
+  function applyCanvasFilters() {
+    if (!imageRef.current) return;
+    const ctx = canvasRef.current.getContext('2d');
+    ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    ctx.filter = filterStringRef.current;
+    ctx.drawImage(imageRef.current, 0, 0);
+  }
+
+  useEffect(() => {
+    applyCanvasFilters();
+  }, [filterString]);
+
+  function resizeCanvas() {
+    if (imageRef.current.src) {
+      adjustImageSize();
+      applyCanvasFilters();
+    }
+  }
+
+  function readDroppedFile({ dataTransfer: { items: [file] } }) {
+    const reader = new FileReader();
+    reader.onload = () => imageRef.current.src = reader.result;
+    reader.readAsDataURL(file.getAsFile());
+    // updateDownloadName(file.getAsFile());
+  }
+
+  function adjustImageSize() {
+    const computedValues = getComputedStyle(dropZoneRef.current);
+    const computedWidth = parseFloat(computedValues.getPropertyValue('width').replace('px', ''));
+    const computedHeight = parseFloat(computedValues.getPropertyValue('height').replace('px', ''));
+    let scaleValue;
+    const canvas = canvasRef.current;
+
+    canvas.width = imageRef.current.width;
+    canvas.height = imageRef.current.height;
+    canvas.style.width = `${canvas.width}px`;
+    canvas.style.height = `${canvas.height}px`;
+
+    let canvasWidth = parseFloat(canvas.style.width.replace('px', ''));
+    let canvasHeight = parseFloat(canvas.style.height.replace('px', ''));
+
+    if (canvasWidth > computedWidth) {
+      scaleValue = computedWidth / canvasWidth;
+      const newWidth = canvasWidth * scaleValue;
+      const newHeight = canvasHeight * scaleValue;
+      canvas.style.width = `${newWidth}px`;
+      canvas.style.height = `${newHeight}px`;
+      canvasWidth = parseFloat(canvas.style.width.replace('px', ''));
+      canvasHeight = parseFloat(canvas.style.height.replace('px', ''));
+    }
+
+    if (canvasHeight > computedHeight) {
+      scaleValue = computedHeight / canvasHeight;
+      const newWidth = canvasWidth * scaleValue;
+      const newHeight = canvasHeight * scaleValue;
+      canvas.style.width = `${newWidth}px`;
+      canvas.style.height = `${newHeight}px`;
+    }
+  }
+
+  useEffect(() => {
+    const dropZone = dropZoneRef.current;
+    const handleDragOver = (e) => e.preventDefault();
+    const handleDrop = (e) => { e.preventDefault(); readDroppedFile(e); };
+    const handleDragIn = (e) => e.preventDefault();
+    const handleDragOut = (e) => e.preventDefault();
+
+    dropZone.addEventListener('dragenter', handleDragIn);
+    dropZone.addEventListener('dragleave', handleDragOut);
+    dropZone.addEventListener('dragover', handleDragOver);
+    dropZone.addEventListener('drop', handleDrop);
+
+    return () => {
+      dropZone.removeEventListener('dragenter', handleDragIn);
+      dropZone.removeEventListener('dragleave', handleDragOut);
+      dropZone.removeEventListener('dragover', handleDragOver);
+      dropZone.removeEventListener('drop', handleDrop);
+    };
+  }, []);
+
+  useEffect(() => {
+    const canvasResizer = new ResizeObserver(resizeCanvas);
+    canvasResizer.observe(dropZoneRef.current);
+    return () => canvasResizer.unobserve(dropZoneRef.current);
+  }, []);
+
+  useEffect(() => {
+    imageRef.current = new Image();
+    imageRef.current.onload = () => {
+      adjustImageSize();
+      applyCanvasFilters(imageRef.current);
+    };
+  }, []);
+
+  return (
+    <Container>
+      <Slider setFilterString={setFilterString} />
+      <ImageContainer ref={dropZoneRef}>
+        <canvas className="drop-zone__canvas" width="0" height="0" ref={canvasRef} />
+        <span className="drop-zone__text">Drop Your Image Here</span>
+      </ImageContainer>
+    </Container>
+  );
 }
