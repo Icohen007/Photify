985a4e050c19e02a584b391c4efde0082f33f57e
Itamar Cohen <itamar.c@taboola.com>
2020-09-26 22:49:30 +0300

    add sidebar and download button

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ min-height: 100vh;
 `;
 
 const ImageContainer = styled.div`
-width: 80%;
+width: 100%;
 display: flex;
 justify-content: center;
 align-items: center;
@@ -23,6 +23,36 @@ position:absolute;
 padding: 20rem;
 border: black 1.5px dashed;
 }
+`;
+
+const Sidebar = styled.section`
+height: 100%;
+width: 200rem;
+display: flex;
+flex-direction: column;
+align-items: center;
+`;
+
+const buttonColor = '#4141ff';
+
+const DownloadButton = styled.a`
+background-color: white;
+padding: 10rem;
+border: 2px solid ${buttonColor};
+outline: none;
+color: ${buttonColor};
+border-radius: 5rem;
+margin-top: 20rem;
+cursor: pointer;
+font-weight: bold;
+font-size: 14rem;
+transition: all ease-in-out 0.2s;
+
+&:hover {
+background-color: ${buttonColor};
+color: white;
+}
+
 `;
 
 const newValuesRatioKeeper = (innerSide1, innerSide2, outerSide) => {
@@ -58,6 +88,7 @@ const Home = () => {
   const dropZoneRef = useRef(null);
   const canvasRef = useRef(null);
   const imageRef = useRef(null);
+  const downloadButtonRef = useRef(null);
   const [filterString, setFilterString, filterStringRef] = useStateRef('');
 
   const applyCanvasFilters = () => {
@@ -103,9 +134,18 @@ const Home = () => {
     applyCanvasFilters();
   }, [filterString]);
 
+  const handleClick = () => {
+    if (imageRef.current.src) {
+      downloadButtonRef.current.href = canvasRef.current.toDataURL();
+    }
+  };
+
   return (
     <Container>
-      <Slider setFilterString={setFilterString} />
+      <Sidebar>
+        <Slider setFilterString={setFilterString} />
+        <DownloadButton ref={downloadButtonRef} download onClick={handleClick}>Download Image</DownloadButton>
+      </Sidebar>
       <ImageContainer
         ref={dropZoneRef}
         onDrop={handleDrop}
